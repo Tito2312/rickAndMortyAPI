b9f2f63a079e0c45787e70891b8a14bf536ff11d
workservices283 <workservices283@gmail.com>
2022-10-13 22:47:00 -0500

    hice los estilos.T23

--- app.js
+++ app.js
@@ -20,21 +20,30 @@ function createCard(data){
 
             const card = document.createElement('div');
             card.classList.add('content')
+
+            const cardContent = document.createElement('div')
+            cardContent.classList.add('cardContent')
             
             const name = document.createElement('h2')
             name.textContent = data.name
             name.classList.add('names')
     
+            const containerImg = document.createElement('div')
+            containerImg.classList.add('containerImg')
+
             const img = document.createElement('img')
             img.src = data.image
             img.classList.add('images')
 
             const species = document.createElement('h5')
             species.textContent = data.species
+            species.classList.add('species')
 
-            card.appendChild(name)
-            card.appendChild(species)
-            card.appendChild(img)
+            card.appendChild(cardContent)
+            card.appendChild(containerImg)
+            cardContent.appendChild(name)
+            cardContent.appendChild(species)
+            containerImg.appendChild(img)
             containerCard.appendChild(card)
         })
 }
